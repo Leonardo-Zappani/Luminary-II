5d0b2634e624adb70a0468f0264d31d48f3a2673
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2024-05-13 20:42:17 -0300

    fixing web token

--- Back Rails/db/schema.rb
+++ Back Rails/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_233425) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_233929) do
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
