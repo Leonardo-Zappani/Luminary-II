4fb255cd7df6b60776d7804861cd4dc7683c4f93
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2024-05-20 19:21:57 -0300

    adding auth register

---
+++ Front React/src/register.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Form, Input, Button, notification } from 'antd';
+import axios from 'axios';
+
+const Register = () => {
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = async (values) => {
+    setLoading(true);
+    try {
+      await axios.post('/api/register', values);
+      notification.success({ message: 'Registration successful!' });
+    } catch (error) {
+      notification.error({ message: 'Registration failed', description: error.message });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Form onFinish={onFinish}>
+      <Form.Item name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+        <Input placeholder="Username" />
+      </Form.Item>
+      <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+        <Input.Password placeholder="Password" />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" loading={loading}>Register</Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Register;
