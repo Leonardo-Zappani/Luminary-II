da980c176a705eca407df1c414873fc38d6f2569
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2024-05-21 20:00:39 -0300

    adding register screen

--- Front React/src/components/Auth/Register.tsx
+++ Front React/src/components/Auth/Register.tsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, notification } from 'antd';
-import axios from 'axios';
+import {Form, Input, Button, notification, Card} from 'antd';
+import {register} from "../../services/apiService";
 
-const Register = () => {
+const Register = ({setToken}) => {
     const [loading, setLoading] = useState(false);
 
     const onFinish = async (values) => {
         setLoading(true);
         try {
-            await axios.post('/api/register', values);
+            await register({...values, new_user: true}).then((result) => {
+                setLoading(false);
+                setToken(result.data);
+                window.location.reload();
+            })
             notification.success({ message: 'Registration successful!' });
         } catch (error) {
             notification.error({ message: 'Registration failed', description: error.message });
@@ -18,17 +22,34 @@ const Register = () => {
     };
 
     return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+            width: '100%',
+            background: '#f0f2f5',
+        }}>
+            <Card title="Seja muito bem vindo!" style={{
+                width: '400px',
+                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+            }}>
         <Form onFinish={onFinish}>
-            <Form.Item name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
-                <Input placeholder="Username" />
+            <Form.Item name="email" rules={[{ required: true, message: 'Por favor, informe seu email!' }]}>
+                <Input placeholder="email" />
             </Form.Item>
-            <Form.Item name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-                <Input.Password placeholder="Password" />
+            <Form.Item name="name" rules={[{ required: true, message: 'Por favor, informe seu nome!' }]}>
+                <Input placeholder="nome" />
+            </Form.Item>
+            <Form.Item name="password" rules={[{ required: true, message: 'Por favor, informe sua senha!' }]}>
+                <Input.Password placeholder="Senha" />
             </Form.Item>
             <Form.Item>
-                <Button type="primary" htmlType="submit" loading={loading}>Register</Button>
+                <Button type="primary" htmlType="submit" loading={loading}>Registrar!</Button>
             </Form.Item>
         </Form>
+            </Card>
+        </div>
     );
 };
 
