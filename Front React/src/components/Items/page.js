1cc0f1c97c0a286124b27954454a442be942196b
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2024-05-22 20:56:25 -0300

    creating front end routes for items

---
+++ Front React/src/components/Items/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import ItemList from './list';
+import ItemForm from './form';
+
+const ItemsPage = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<ItemList />} />
+            <Route path="/create" element={<ItemForm />} />
+            <Route path="/edit/:id" element={<ItemForm />} />
+        </Routes>
+    );
+};
+
+export default ItemsPage;
