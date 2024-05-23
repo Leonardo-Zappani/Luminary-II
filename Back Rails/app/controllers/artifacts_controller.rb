3f9e0f5df7f4c735817a2c1c62416dd71c806ed3
Leonardo-Zappani <leonardozappani@alunos.utfpr.edu.br>
2024-05-23 20:02:55 -0300

    adding DTOs

--- Back Rails/app/controllers/artifacts_controller.rb
+++ Back Rails/app/controllers/artifacts_controller.rb
@@ -4,18 +4,35 @@ class ArtifactsController < ApplicationController
   # GET /artifacts
   def index
     @artifacts = Artifact.all
+    payload = @artifacts.map do |artifact|
+      {
+        id: artifact.id,
+        name: artifact.name,
+        description: artifact.description,
+        assigned_names: artifact.user_names,
+        item_names: artifact.item_names,
+      }
+    end
 
-    render json: @artifacts
-  end
+    render json: payload
+    end
+
 
   # GET /artifacts/1
   def show
-    render json: @artifact
+    payload = {
+      id: @artifact.id,
+      name: @artifact.name,
+      description: @artifact.description,
+      assigned_names: @artifact.user_ids&.split(','),
+      item_names: @artifact.item_ids&.split(','),
+    }
+    render json: payload
   end
 
   # POST /artifacts
   def create
-    @artifact = Artifact.new(description: params[:description], name: params[:name])
+    @artifact = Artifact.new(description: params[:description], name: params[:name], user_ids: params[:users].join(','), item_ids: params[:items].join(','))
 
     if @artifact.save
       render json: @artifact, status: :created, location: @artifact
@@ -26,7 +43,7 @@ class ArtifactsController < ApplicationController
 
   # PATCH/PUT /artifacts/1
   def update
-    if @artifact.update(description: params[:description], name: params[:name])
+    if @artifact.update(description: params[:description], name: params[:name], user_ids: params[:users].join(','), item_ids: params[:items].join(','))
       render json: @artifact
     else
       render json: @artifact.errors, status: :unprocessable_entity
